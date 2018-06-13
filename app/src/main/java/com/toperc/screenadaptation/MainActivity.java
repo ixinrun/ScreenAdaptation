33bedf592472232170b52b84efbd1ba63a2f274e
toperc <runxinluo2015@163.com>
2018-06-14 00:00:25 +0800

    no message

--- app/src/main/java/com/toperc/screenadaptation/MainActivity.java
+++ app/src/main/java/com/toperc/screenadaptation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.toperc.screenadaptation;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DensityUtil.resetDensity(this);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        DensityUtil.resetDensity(this);
     }
 }
